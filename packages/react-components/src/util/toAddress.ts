11c61cbedc31042629bf22261e1dea2b4dc732e2
TarasPotsikaylo <potsikaylot@gmail.com>
2023-03-28 19:51:51 +0300

    added EVM to Substrate address converter

--- packages/react-components/src/util/toAddress.ts
+++ packages/react-components/src/util/toAddress.ts
@@ -3,7 +3,7 @@
 
 import { keyring } from '@polkadot/ui-keyring';
 import { hexToU8a, isHex } from '@polkadot/util';
-import { ethereumEncode } from '@polkadot/util-crypto';
+import { ethereumEncode, evmToAddress, isEthereumAddress } from '@polkadot/util-crypto';
 
 export function toAddress (value?: string | Uint8Array | null, allowIndices = false, bytesLength?: 20 | 32): string | undefined {
   if (value) {
@@ -11,6 +11,10 @@ export function toAddress (value?: string | Uint8Array | null, allowIndices = fa
       const u8a = isHex(value)
         ? hexToU8a(value)
         : keyring.decodeAddress(value);
+
+      if (isHex(value) && isEthereumAddress(value)) {
+        return evmToAddress(value, 42);
+      }
 
       if (!allowIndices && u8a.length !== 32 && u8a.length !== 20) {
         throw new Error('AccountIndex values not allowed');
